e8f4290d54bd4d39c03ddd5d389926b12c3c96cd
carloslennix <carloslennix38@gmail.com>
2026-03-05 16:59:21 -0400

    auth fix

--- components/auth.tsx
+++ components/auth.tsx
@@ -3,6 +3,7 @@ import { Alert, StyleSheet, View, AppState } from 'react-native'
 import { supabase } from '../lib/supabase'
 import { Button, Input } from 'react-native-elements'
 
+
 // Tells Supabase Auth to continuously refresh the session automatically if
 // the app is in the foreground. When this is added, you will continue to receive
 // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
